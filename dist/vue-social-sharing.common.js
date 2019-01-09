0365d5c9453b3631ca4390fa6964b1cc71c6a532
kdes70 <kdes70work@gmail.com>
2019-01-09 20:29:52 +0300

    fix fb image

--- dist/vue-social-sharing.common.js
+++ dist/vue-social-sharing.common.js
@@ -1,6 +1,6 @@
 /*!
  * vue-social-sharing v2.3.3 
- * (c) 2018 nicolasbeauvais
+ * (c) 2019 nicolasbeauvais
  * Released under the MIT License.
  */
 'use strict';
@@ -50,7 +50,7 @@ var SocialSharingNetwork = {
 };
 
 var email = {"sharer":"mailto:?subject=@title&body=@url%0D%0A%0D%0A@description","type":"direct"};
-var facebook = {"sharer":"https://www.facebook.com/sharer/sharer.php?u=@url&title=@title&description=@description&quote=@quote","type":"popup"};
+var facebook = {"sharer":"https://www.facebook.com/sharer/sharer.php?u=@url&title=@title&picture=@media&description=@description&quote=@quote","type":"popup"};
 var googleplus = {"sharer":"https://plus.google.com/share?url=@url","type":"popup"};
 var line = {"sharer":"http://line.me/R/msg/text/?@description%0D%0A@url","type":"popup"};
 var linkedin = {"sharer":"https://www.linkedin.com/shareArticle?mini=true&url=@url&title=@title&summary=@description","type":"popup"};
